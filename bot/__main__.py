a3d072f065a0c5a7f676cecae02da926fd7e967f
A N I K E T <86464839+Aniket-Code10010@users.noreply.github.com>
2022-06-11 13:06:08 +0530

    Add files via upload

--- bot/__main__.py
+++ bot/__main__.py
@@ -14,14 +14,14 @@ async def strt(event):
     if str(event.sender_id) not in OWNER:
         return
     btn = [
-        [Button.inline("ADD NEW MSG PAIR", data="addmsg")],
         [Button.inline("RESTART BOT", data="restart")],
         [
             Button.inline("Start Promo", data="startpromo"),
             Button.inline("Stop Promo", data="stoppromo"),
-        ]
+        ],
     ]
     await event.reply("Choose Options", buttons=btn)
+
 
 @bot.on(events.NewMessage(incoming=True, pattern="^/help"))
 async def help(e):
@@ -31,8 +31,6 @@ async def help(e):
 • `/addch <id>` - __This Will add Channel id__
 
 • `/remch <id>` - __This Will Remove Channel id__
-
-• `/remmsgpair` - __To Delete the ad__
 
 • `/interval <in seconds>` - __To set Interval of ad__
 """
@@ -44,13 +42,17 @@ async def addchh(event):
     if str(event.sender_id) not in OWNER:
         return
     try:
-        id = event.text.split()[1]
+        id = int(event.text.split()[1])
     except:
         return await event.reply("Input Not Found")
-    chs = dB.get("CHATS") or []
-    if int(id) not in chs:
-        chs.append(int(id))
-    dB.set("CHATS", chs)
+    chs = dB.get("AD_DATA") or {}
+    async with bot.conversation(event.sender_id, timeout=500) as cv:
+        await cv.send_message(f"Send 1st Promotion Ad Of {id} to post on other Chats.")
+        repl = (await cv.get_response()).text
+        await cv.send_message(f"Send 2nd Promotion Ad Of {id} to post on other Chats.")
+        repl2 = (await cv.get_response()).text
+        chs[id] = [repl, repl2]
+    dB.set("AD_DATA", chs)
     await event.reply("`Succesfully Added`")
 
 
@@ -59,60 +61,14 @@ async def remmchh(event):
     if str(event.sender_id) not in OWNER:
         return
     try:
-        id = event.text.split()[1]
+        id = int(event.text.split()[1])
     except:
         return await event.reply("Input Not Found")
-    chs = dB.get("CHATS") or []
-    if int(id) in chs:
-        chs.remove(int(id))
-    dB.set("CHATS", chs)
+    chs = dB.get("AD_DATA") or {}
+    if id in chs:
+        chs.pop(id)
+    dB.set("AD_DATA", chs)
     await event.reply("`Succesfully Removed`")
-
-
-@bot.on(events.callbackquery.CallbackQuery(data=re.compile("addmsg")))
-async def add_msgs(event):
-    ADS = dB.get("ADS") or []
-    async with event.client.conversation(event.sender_id) as cv:
-        await cv.send_message(
-            """`Send First Message of the pair`\n__Note: Avoid to use " and ' in message.__"""
-        )
-        msg1 = await cv.get_response(timeout=90)
-        if msg1.text.startswith("/cancel"):
-            return await msg1.reply("`Process Cancelled Successfully`")
-        await cv.send_message(
-            """`Send Second Message of the pair`\n__Note: Avoid to use " and ' in message.__"""
-        )
-        msg2 = await cv.get_response(timeout=90)
-        if msg2.text.startswith("/cancel"):
-            return await msg2.reply("`Process Cancelled Successfully`")
-        xx = [msg1.text, msg2.text]
-        ADS.append(xx)
-        dB.set("ADS", ADS)
-        return await cv.send_message("`Succesfully Added`")
-
-
-@bot.on(events.NewMessage(incoming=True, pattern="^/remmsgpair"))
-async def rem_msg_pair(event):
-    if str(event.sender_id) not in OWNER:
-        return
-    ADS = dB.get("ADS") or []
-    i = 0
-    text = ""
-    for item in ADS:
-        text += f"`{i}` - `{item}`\n"
-        i += 1
-    if not text:
-        return await event.reply("`No Msg Found`")
-    async with event.client.conversation(event.sender_id) as cv:
-        await cv.send_message(text)
-        await cv.send_message("`Send The Corresponding numbers of the ad`")
-        no = await cv.get_response(timeout=90)
-        x = await cv.send_message("`Processing...`")
-        if no.text.startswith("/cancel"):
-            return await x.reply("`Process Cancelled Successfully`")
-        ADS.pop(int(no.text))
-        dB.set("ADS", ADS)
-        return await x.edit("`Done.`")
 
 
 @bot.on(events.NewMessage(incoming=True, pattern="^/interval"))
@@ -123,7 +79,7 @@ async def intt(event):
         inn = event.text.split()[1]
     except:
         return await event.reply("`Invalid Input`")
-    dB.set("INTERVAL", int(inn))
+    dB.set("INTERVAL1", int(inn))
     await event.reply("`Done.`")
 
 
@@ -158,56 +114,52 @@ async def _(e):
 @bot.on(events.callbackquery.CallbackQuery(data=re.compile("restart")))
 async def restart(event):
     x = await event.reply("`Restarting...`")
-    dB.set("RESTART", [x.id, x.chat_id])
+    dB.set("RESTART1", [x.id, x.chat_id])
     os.execl(sys.executable, sys.executable, "-m", "bot")
 
 
 async def onstart():
     try:
-        xx = dB.get("RESTART")
+        xx = dB.get("RESTART1")
         x = await bot.get_messages(xx[1], ids=xx[0])
         await x.edit("`Restarted`")
-        dB.delete("RESTART")
+        dB.delete("RESTART1")
     except BaseException:
-        dB.delete("RESTART")
+        dB.delete("RESTART1")
 
 
 async def msg_poster():
-    INTERVAL = dB.get("INTERVAL")
-    CHATS = dB.get("CHATS")
-    ADS = dB.get("ADS")
-    chat1 = CHATS[0]
-    CHATS.pop(0)
-    sent_to = []
-    while len(sent_to) != len(CHATS):
-        n = 0
-        for chat in CHATS:
-            if len(ADS) < (n + 1):
-                break
-            msgpair1 = ADS[n]
-            msgpair2 = None
-            msgpair2 = ADS[n + 1]
-            n += 1
-            if chat not in sent_to:
-                sent_to.append(chat)
-            else:
-                continue
+    INTERVAL = dB.get("INTERVAL1") or dB.get("INTERVAL") or 3600
+    DATA = dB.get("AD_DATA")
+    CROSS = []
+    for chat1 in list(DATA):
+        for chat2 in list(DATA):
+            if chat1 != chat2:
+                CROSS.append([chat1, chat2])
+    if CROSS:
+        for chat1, chat2 in list(CROSS):
             try:
-                sent_1 = await bot.send_message(chat, msgpair1[0])
-                sent_2 = await bot.send_message(chat, msgpair1[1])
-                sent_3 = await bot.send_message(chat1, msgpair2[0])
-                sent_4 = await bot.send_message(chat1, msgpair2[1])
-                # could've used apscheduler here, but going for an easier approach.
+                sent = []
+                msg1 = DATA[chat2] if isinstance(DATA[chat2], list) else [DATA[chat2]]
+                msg2 = DATA[chat1] if isinstance(DATA[chat1], list) else [DATA[chat1]]
+                for m in msg1:
+                    sent.append(await bot.send_message(chat1, m))
+                for m in msg2:
+                    sent.append(await bot.send_message(chat1, m))
                 await asyncio.sleep(int(INTERVAL))
-                await sent_1.delete()
-                await sent_2.delete()
-                await sent_3.delete()
-                await sent_4.delete()
+                for snt in sent:
+                    try:
+                        await snt.delete()
+                    except:
+                        pass
             except Exception as exc:
                 log.error(exc)
-                log.error("Failed to send/delete message in %s.", chat)
-        if len(sent_to) == len(CHATS):
-            sent_to = []
+                log.error("Failed to send message in {} {}".format(chat1, chat2))
+                for snt in sent:
+                    try:
+                        await snt.delete()
+                    except:
+                        pass
 
 
 log.info("Started bot, looping cross promoter messages.")
